dfd22173ebc052e3d43652719759922cb9803e79
karimi-george <kimamogeorge69@gmail.com>
2024-06-29 13:24:08 +0300

    update

--- weather1/views.py
+++ weather1/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'weather/index.html') #returns the index.html template
+        url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=8c59428e6f5bb0ce710e1cabd82f1ec4'
+
+    city = 'Las Vegas'
+
+    city_weather = requests.get(url.format(city)).json() #request the API data and convert the JSON to Python data types
+    return render(request, 'weather1/index.html') #returns the index.html template
